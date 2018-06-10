e6fbf49ae4e847ed78d0a5b22ca3e5c0d861dd33
Denys Vasyliev [GL] <Denys.Vasyliev@qsc.com>
2018-06-10 16:15:02 +0300

    remove new feature

--- main.go
+++ main.go
@@ -111,7 +111,7 @@ func greetingsID(decodedStr string) string {
 		Password: "", // no password set
 		DB:       0,  // use default DB
 	})
-	decodedStr = "new feature"
+	//decodedStr = "new feature"
 	log.Print("DecodedStr: ", decodedStr)
 	encodedStr := hex.EncodeToString([]byte(banner.PrintS(decodedStr)))
 	log.Print("EncodedStr: ", encodedStr)
