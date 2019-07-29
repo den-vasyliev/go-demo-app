1d2dc448722c8c162c9003089014554a0e627a4b
Denys Vasyliev <den@msrn.me>
2019-07-29 23:02:04 +0300

    some clean

--- tf/gke.tf
+++ tf/gke.tf
@@ -1,27 +1,33 @@
 provider "google" {
-  project = "smartcity-gl"
-  region  = "europe-west2"
-  zone    = "europe-west2-a"
+  credentials = "${file("/Users/den/code/gcp/cred-gke.json")}"
+  project     = "smartcity-gl"
+  region      = "europe-west2"
+  zone        = "europe-west2-a"
 }
 
 resource "google_container_cluster" "primary" {
-  name                     = "gke-example"
+  name                     = "demo"
   location                 = "europe-west2-a"
   remove_default_node_pool = true
+  initial_node_count       = 1
 
-  node_pool {
-    name = "default-pool"
-  }
+
 }
 
-resource "google_container_node_pool" "primary_pool" {
+resource "google_container_node_pool" "primary-pool" {
   name       = "primary-pool"
   cluster    = "${google_container_cluster.primary.name}"
   location   = "europe-west2-a"
   node_count = "2"
 
   node_config {
-    machine_type = "n1-standard-1"
+    preemptible  = true
+    machine_type = "n1-standard-2"
+
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
   }
 
   autoscaling {
@@ -35,23 +41,3 @@ resource "google_container_node_pool" "primary_pool" {
   }
 }
 
-resource "google_container_node_pool" "new_pool" {
-  name       = "new-pool"
-  cluster    = "${google_container_cluster.primary.name}"
-  location   = "europe-west2-a"
-  node_count = "4"
-
-  node_config {
-    machine_type = "n1-standard-2"
-  }
-
-  autoscaling {
-    min_node_count = 2
-    max_node_count = 6
-  }
-
-  management {
-    auto_repair  = true
-    auto_upgrade = true
-  }
-}
